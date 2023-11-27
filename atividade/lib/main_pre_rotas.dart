425909ab528e373dc3757b01e6cbae4e20a14261
Pedro Henrique Silva Costa <pedritohscostaph67@gmail.com>
2023-11-27 19:54:26 -0300

    Atividade 2 4bim

---
+++ atividade/lib/main_pre_rotas.dart
@@ -0,0 +1,28 @@
+import 'package:atividade/Telas/tela_produtos.dart';
+import 'package:atividade/utils/rotas.dart';
+import 'package:flutter/material.dart';
+
+//depois de programar a tela
+import 'Telas/tela_categoria.dart';
+
+void main() => runApp(AppCardapio());
+
+// style: Theme.of(context).textTheme.titleSmall, vai no categoria
+class AppCardapio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Cardápio',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            fontFamily: 'Schyler',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                titleSmall:
+                    const TextStyle(fontSize: 20, fontFamily: "Schyler"))),
+        //home: TelaCategorias(),
+        routes: {
+          Rotas.HOME: (ctx) => TelaCategorias(),
+          Rotas.PRODUTOS: (ctx) => TelaProdutos()
+        });
+  }
+}
